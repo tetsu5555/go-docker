a8744548452fcbf9a76ad98719fefd655e3e1647
tetsu5555 <tetete1118@gmail.com>
2019-06-22 19:03:21 +0900

    fix

--- backend/main.go
+++ backend/main.go
@@ -2,33 +2,25 @@ package main
 
 import (
 	// "github.com/jinzhu/gorm"
-	// _ "github.com/jinzhu/gorm/dialects/mysql"
-	// "github.com/labstack/echo/v4"
-	// "github.com/labstack/echo/v4/middleware"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 
-	"os/exec"
-	"fmt"
-
-	// "backend/handlers"
+	"backend/handlers"
 )
 
 func main() {
-	// // Echo instance
-	// 		e := echo.New()
+	// Echo instance
+			e := echo.New()
 
-	// // Middleware
-	// 		e.Use(middleware.Logger())
-	// 	e.Use(middleware.Recover())
+	// Middleware
+			e.Use(middleware.Logger())
+		e.Use(middleware.Recover())
 
-	// // Routes
-	// 	e.GET("/", handlers.Hello)
+	// Routes
+		e.GET("/", handlers.Hello)
 
-	// // Start server
-	// 	e.Logger.Fatal(e.Start(":1313"))
+	// Start server
+		e.Logger.Fatal(e.Start(":1313"))
 
-	b, err := exec.Command("git", "status").Output()
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(b)
 }
