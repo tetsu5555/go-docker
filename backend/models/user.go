16e883406bbf909bfe3ee608b12062d7d55b106b
tetsu5555 <tetete1118@gmail.com>
2019-06-27 14:31:06 +0900

    delete id property from user

--- backend/models/user.go
+++ backend/models/user.go
@@ -3,7 +3,6 @@ package models
 import "github.com/jinzhu/gorm"
 
 type User struct {
-	id int
 	gorm.Model
 	Name     string `gorm:"type:varchar(100);unique_index" db:"name" json:"name"`
 	Password string `db:"password" json:"password"`
