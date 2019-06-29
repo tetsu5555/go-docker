75195fd66677b3b53857e213e853413050788196
tetsu5555 <tetete1118@gmail.com>
2019-06-29 17:53:08 +0900

    delete lint errorgs

--- backend/example.go
+++ backend/example.go
@@ -6,7 +6,8 @@ import (
 
 func example(code string) (int, error) {
 	if code == "hoge" {
-			return 1, nil
+		return 1, nil
 	}
-		return 0, errors.New("code must be hoge")
+
+	return 0, errors.New("code must be hoge")
 }
